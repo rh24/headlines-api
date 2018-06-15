b4ec39fcdca50da4537e412590f49a952e9b3f95
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-15 17:56:38 -0400

    Add StoryDashboard component and render it in parent App component via react-router

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StoryDashboard from './containers/StoryDashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route path='/stories' component={StoryDashboard} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/containers/StoryDashboard.js
+++ client/src/containers/StoryDashboard.js
@@ -1,2 +1,20 @@
 // <StoryDashboard /> will be a stateful class component.
 // <StoryCard /> will be a child, stateless functional/presentation component.
+
+import React from 'react';
+
+class StoryDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="story-dashboard">
+        <h1>HelloWorld</h1>
+      </div>
+    );
+  }
+}
+
+export default StoryDashboard;
