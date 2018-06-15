9ca420068b16893684d6f82b32294168b1159387
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-15 17:41:36 -0400

    Add material-ui grid container and items to style navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const NavBar = () => {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        My Stories
-      </Button>
-      Hi
+    <div style={{ padding: 20 }}>
+      <Grid container className="navbar" justify='center'>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            My Stories
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            Locations
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            Categories
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
