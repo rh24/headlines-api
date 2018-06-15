5600ff133e5bec9fa9c1c0018038cf33cad08518
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-15 16:59:13 -0400

    Add material-ui and react-dom dependencies

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const NavBar = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        My Stories
+      </Button>
+      Hi
+    </div>
+  );
+}
+
+export default NavBar;
