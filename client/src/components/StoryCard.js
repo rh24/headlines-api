504ed3fe953fc9490c7d9c2ce6e8ca33864a4193
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-18 23:21:31 -0400

    Displays each headline in material UI card

--- client/src/components/StoryCard.js
+++ client/src/components/StoryCard.js
@@ -1,19 +1,26 @@
 // StoryCard is basically a headlines card but styled to be bigger than a <HeadlinesCard />, which is a child of the <LocationCard /> component.
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+// import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class StoryCard extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  render() {
-    return (
-      <Grid container className="story-cards">
-        Title: {this.props.story.title}
+const StoryCard = (props) => {
+  return (
+    <Grid container className="story-cards" spacing={8}>
+      <Grid item xs={12}>
+        <Card>
+          {props.story.urlToImage}
+          {props.story.title}
+        </Card>
       </Grid>
-    )
-  }
+    </Grid>
+  )
 }
 
 export default StoryCard;
