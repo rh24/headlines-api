824847457bb730e47697776154a6694b91041b28
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-20 18:09:37 -0400

    Change page size of fetched data to display 100 results

--- client/src/actions/stories.js
+++ client/src/actions/stories.js
@@ -3,7 +3,7 @@ const apiKey = '7b48baa7e48541b9af219e981533d442';
 export function fetchStories() {
   return (dispatch) => {
     // dispatch({ type: 'FETCH_STORIES', stories });
-    return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+    return fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=${apiKey}`)
       .then(resp => resp.json())
       .then(json => json.articles)
       .then((stories) => {
