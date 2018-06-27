22906bd9622372ffc0b3f788f6c0ffd821c042e6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 16:31:03 -0400

    Change instance method name to prevent infinite loop when returning news_api_id attribute

--- app/models/source.rb
+++ app/models/source.rb
@@ -13,15 +13,16 @@ class Source < ApplicationRecord
   #     end
   #   end
   # end
-  
+
   # Why wasn't the above class method working?
 
-  def news_api_id
+  def get_news_api_id
     if self.name.include?(".com")
       self.news_api_id = null
     else
       self.news_api_id = self.name.downcase.gsub!(/\s/, '-')
       self.save
+      news_api_id
     end
   end
 end
