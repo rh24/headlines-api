6910f7febaa083d59b381280ed81079e90a3ef5b
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 14:48:56 -0400

    Change identifier of storiesReducer function in its file to match all identifiers referring to this function elsewhere in the app

--- client/src/reducers/storiesReducer.js
+++ client/src/reducers/storiesReducer.js
@@ -1,4 +1,4 @@
-export default function fetch(state = [], action) {
+export default function storiesReducer(state = [], action) {
   switch (action.type) {
     case 'FETCH_TOP_STORIES':
       return action.stories;
