5dbc037593a4c9fde6f6778b1b94a17bf460c9c8
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 15:58:07 -0400

    Change news_api_id instance method into a class method in order to execute once in the console

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,12 +2,16 @@ class Source < ApplicationRecord
   has_many :stories
   belongs_to :category
   has_many :users, through: :stories
+  attr_accessor :news_api_id
 
-  def news_api_id
-    if self.name.include?(".com")
-      self.news_api_id = null
-    else
-      self.news_api_id = self.name.downcase.gsub!(/\s/, '-')
+  def self.news_api_id
+    self.all.each do |source|
+      if source.name.include?(".com")
+        source.news_api_id = null
+      else
+        source.news_api_id = source.name.downcase.gsub!(/\s/, '-')
+        source.save
+      end
     end
   end
 end
