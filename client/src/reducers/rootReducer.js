3368b12fb6b5d98ca94ecaafcc68747c0e1699c6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 14:47:11 -0400

    Add categories reducer to root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import storiesReducer from './storiesReducer';
 import keywordReducer from './keywordReducer';
 import sourcesReducer from './sourcesReducer';
+import categoriesReducer from './categoriesReducer';
 
 const rootReducer = combineReducers({
   stories: storiesReducer,
   searchedStories: keywordReducer,
-  sources: sourcesReducer
+  sources: sourcesReducer,
+  categories: categoriesReducer
 })
 
 export default rootReducer;
