6975263c636b097513201e7394a501bd6534460a
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 16:06:17 -0400

    Fix seed data and change class method back to instance method

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,16 +2,26 @@ class Source < ApplicationRecord
   has_many :stories
   belongs_to :category
   has_many :users, through: :stories
-  attr_accessor :news_api_id
 
-  def self.news_api_id
-    self.all.each do |source|
-      if source.name.include?(".com")
-        source.news_api_id = null
-      else
-        source.news_api_id = source.name.downcase.gsub!(/\s/, '-')
-        source.save
-      end
+  # def self.news_api_id
+  #   self.all.each do |source|
+  #     if source.name.include?(".com")
+  #       source.news_api_id = null
+  #     else
+  #       source.news_api_id = source.name.downcase.gsub!(/\s/, '-')
+  #       source.save
+  #     end
+  #   end
+  # end
+  
+  # Why wasn't the above class method working?
+
+  def news_api_id
+    if self.name.include?(".com")
+      self.news_api_id = null
+    else
+      self.news_api_id = self.name.downcase.gsub!(/\s/, '-')
+      self.save
     end
   end
 end
