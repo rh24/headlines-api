6dac2dd5154978ddbc1a4101d47c1f64a6d9f47c
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-27 15:45:52 -0400

    Fix news_api_id instance method

--- app/models/source.rb
+++ app/models/source.rb
@@ -7,7 +7,7 @@ class Source < ApplicationRecord
     if self.name.include?(".com")
       self.news_api_id = null
     else
-      self.news_api_id = self.name.downcased.gsub!(/\s/, '-')
+      self.news_api_id = self.name.downcase.gsub!(/\s/, '-')
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621230823) do
+ActiveRecord::Schema.define(version: 20180627194316) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180621230823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "news_api_id"
+    t.integer "category_id"
   end
 
   create_table "stories", force: :cascade do |t|
