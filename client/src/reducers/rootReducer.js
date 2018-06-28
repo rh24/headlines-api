6bf78dce4d0606f4877a8425d577133b0df6f495
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-28 13:16:07 -0400

    Stub out reducer to manage and update user's saved stories

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import keywordReducer from './keywordReducer';
 import sourcesReducer from './sourcesReducer';
 import categoriesReducer from './categoriesReducer';
 import userReducer from './userReducer';
+import savedStoriesReducer from './savedStoriesReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   stories: storiesReducer,
   searchedStories: keywordReducer,
+  savedStories: savedStoriesReducer,
   sources: sourcesReducer,
   categories: categoriesReducer
 })
---
+++ client/src/reducers/savedStoriesReducer.js
@@ -0,0 +1,6 @@
+export default function savedStoriesReducer(state = [], action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
