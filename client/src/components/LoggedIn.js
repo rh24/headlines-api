44d6795b277f1af3f3e201214fe7ede0bf4596ff
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-28 11:20:11 -0400

    Style logged in component to display user in bold

--- client/src/components/LoggedIn.js
+++ client/src/components/LoggedIn.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 const LoggedIn = (props) => {
   // debugger;
   return ([
-    <p>Logged in as {props.username}.</p>,
+    <p>Logged in as <strong>{props.username}</strong>.</p>,
     <Button onClick={() => this.handleClick()}>
       Switch Account
     </Button>
