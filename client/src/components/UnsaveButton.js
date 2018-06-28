c77bc52a46689467f55956b9d83b3c94b87ac8fd
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-28 18:19:32 -0400

    Add unsave button stateless component

---
+++ client/src/components/UnsaveButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+const UnsaveButton = (props) => {
+  return (
+    <div className="favorite">
+      <Button onClick={(event) => props.handleSave(event)}>Remove Story</Button>
+    </div>
+  )
+}
+
+export default UnsaveButton;
