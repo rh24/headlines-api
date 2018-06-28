385ae28d982710ff64f42866b93cf2805c57b13f
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-28 12:34:05 -0400

    Outline stories controller actions

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,24 @@
 class StoriesController < ApplicationController
+
+  def create
+    
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_user
+
+  end
 end
