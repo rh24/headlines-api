b2c66d718b33778a61421ee39c45804996ff98b9
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-28 10:09:40 -0400

    Add fetchUser dispatch action to fetch user state of existing users

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -5,8 +5,17 @@ export function fetchUserStories(userId) {
     return fetch(`http://localhost:3001/users/${userId}`)
       .then(resp => resp.json())
       .then(userArray => userArray.find((user) => user.id === userId))
-      .then(user => dispatch({ type: 'FETCH_USER_STORIES', stories: user.stories }))
-  }
+      .then(user => dispatch({ type: 'FETCH_USER_STORIES', stories: user.stories }));
+  };
+}
+
+export function fetchUser(username) {
+  return (dispatch) => {
+    return fetch(`http://localhost:3001/users`)
+      .then(resp => resp.json())
+      .then(userArray => userArray.find((user) => user.username === username))
+      .then(user => dispatch({ type: 'SET_USER_STATE', user: user }));
+  };
 }
 
 export function createUser(username) {
@@ -24,6 +33,6 @@ export function createUser(username) {
     return fetch(`http://localhost:3001/users/`, args)
       .then(resp => resp.json())
       .catch(error => console.log(error))
-      .then(user => dispatch({ type: 'SET_USER_STATE', user: user }))
-  }
+      .then(user => dispatch({ type: 'SET_USER_STATE', user: user }));
+  };
 }
