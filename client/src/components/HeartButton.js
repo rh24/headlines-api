4236c4f79a05ae68fc78c23c832fac27ecd7296b
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-29 18:05:32 -0400

    Add heart button component

---
+++ client/src/components/HeartButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import Heart from 'mui-icons/cmdi/heart';
+
+const HeartButton = () => {
+  <IconButton>
+    <Heart />
+  </IconButton>
+}
+
+export default HeartButton;
