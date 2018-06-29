46befcb63f9e8633d0706c48af6c95dc98049db6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-29 15:46:48 -0400

    Add create method in user_categories controller

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -2,7 +2,12 @@ class UserCategoriesController < ApplicationController
   before_action :set_user
 
   def create
-    # user_category = UserCategory.new(user: )
+    user_category = UserCategory.new(user_category_params)
+    if user_category.save
+      render json: user_category, status: 200
+    else
+      render json: user_category.errors, status: 400
+    end
   end
 
   def index
@@ -15,6 +20,10 @@ class UserCategoriesController < ApplicationController
   end
 
   private
+
+  def user_category_params
+    params.require(:user_category).permit(:user_id, :category_id)
+  end
 
   def set_user
     @user = User.find_by(id: params[:id])
